26d0e0231baf56818bc52319a01152be3851f159
msyafiqq <engineering@GBs-MacBook-Pro-4.local>
2023-06-29 20:49:38 +0700

    GB-4: :sparkles: Add Negotiator interface

---
+++ Sources/GBPeerJs/GBPeerJs/Presentation/Interface/INegotiator.swift
@@ -0,0 +1,28 @@
+//
+// Created by engineering on 29/6/23.
+//
+
+import Foundation
+import WebRTC
+import RxSwift
+
+protocol INegotiator: AnyObject {
+    func startConnection(
+            stream: GBPeerMediaStream?,
+            originator: Bool,
+            mediaOfferConstraint: RTCMediaConstraints,
+            remoteOfferSdp: String,
+            remoteOfferSdpType: String
+    ) -> Completable
+
+    func handleSDP(
+            type: String,
+            sdp: String,
+            sdpType: String,
+            mediaOfferConstraint: RTCMediaConstraints
+    ) -> Completable
+
+    func handleCandidate(_ ice: RTCIceCandidate) -> Completable
+
+    func cleanup()
+}

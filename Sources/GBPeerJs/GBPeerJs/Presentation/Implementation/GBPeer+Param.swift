a0fb069c4b55337426ec9a1ff1bf486a6dd97eaa
msyafiqq <engineering@GBs-MacBook-Pro-4.local>
2023-06-30 06:12:03 +0700

    GB-8: :sparkles: Add peerjs implementation
    (cherry picked from commit 0ddd90ad05607223d5581fa87c242b92bba3fc7f)

---
+++ Sources/GBPeerJs/GBPeerJs/Presentation/Implementation/GBPeer+Param.swift
@@ -0,0 +1,38 @@
+//
+// Created by engineering on 27/6/23.
+//
+
+import Foundation
+
+extension GBPeer {
+    public struct PeerOptions {
+        let debug: Bool
+        let host: String?
+        let port: Int?
+        let path: String?
+        let key: String?
+        let token: String?
+        let secure: Bool
+        let pingInterval: TimeInterval?
+
+        public init(
+                debug: Bool,
+                host: String? = nil,
+                port: Int? = nil,
+                path: String? = nil,
+                key: String? = nil,
+                token: String? = nil,
+                secure: Bool,
+                pingInterval: TimeInterval? = nil
+        ) {
+            self.debug = debug
+            self.host = host
+            self.port = port
+            self.path = path
+            self.key = key
+            self.token = token
+            self.secure = secure
+            self.pingInterval = pingInterval
+        }
+    }
+}

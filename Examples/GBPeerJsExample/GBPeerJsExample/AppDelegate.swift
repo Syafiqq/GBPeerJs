5a1148335062b17e20fb3c3171ba0d3a0d4a004c
msyafiqq <engineering@GBs-MacBook-Pro-4.local>
2023-06-30 08:20:17 +0700

    Dev: Install lifetime tracker Example

--- Examples/GBPeerJsExample/GBPeerJsExample/AppDelegate.swift
+++ Examples/GBPeerJsExample/GBPeerJsExample/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import LifetimeTracker
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -27,7 +28,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
             // swiftlint:disable:previous discouraged_optional_collection
     ) -> Bool {
-        true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            LifetimeTracker.setup(
+                    onUpdate: LifetimeTrackerDashboardIntegration(
+                            visibility: .alwaysVisible,
+                            style: .circular
+                    ).refreshUI
+            )
+        }
+        return true
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {

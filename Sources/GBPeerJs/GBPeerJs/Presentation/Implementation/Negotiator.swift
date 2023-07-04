42ceb60f7daaeb8405bc54b42dbf5a18772503bb
msyafiqq <engineering@GBs-MacBook-Pro-4.local>
2023-07-04 07:12:54 +0700

    GB-11: :bug: Mark peer connection close to async

--- Sources/GBPeerJs/GBPeerJs/Presentation/Implementation/Negotiator.swift
+++ Sources/GBPeerJs/GBPeerJs/Presentation/Implementation/Negotiator.swift
@@ -225,7 +225,9 @@ private extension Negotiator {
         }*/
 
         if peerConnectionNotClosed || dataChannelNotClosed {
-            peerConnection?.close()
+            DispatchQueue.main.async {
+                peerConnection?.close()
+            }
         }
     }
 
@@ -782,6 +784,7 @@ extension Negotiator: RTCPeerConnectionDelegate {
     }
 
     func peerConnection(_ peerConnection: RTCPeerConnection, didChange newState: RTCIceConnectionState) {
+        logger.log("peerConnection - didChange -\(newState)")
         switch peerConnection.iceConnectionState {
         case .failed:
             logger.log("iceConnectionState is failed, closing connections to \(connection?.peer ?? "-")")
